edc1f3cd65ebd57fe1757a884a842b195757a850
rodrigompf <111639927+rodrigompf@users.noreply.github.com>
2024-04-14 09:08:11 +0100

    Image

--- app/src/main/java/com/example/hotcue/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hotcue/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -38,5 +39,8 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
+
     }
+
 }
