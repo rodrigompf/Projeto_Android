fe802d0bbfa5f27f1912c73d86c4c56ae9393bbe
vanori <alexvaarino7@gmail.com>
2024-04-17 16:07:33 +0100

    drawer + list

---
+++ app/src/main/java/com/example/hotcue/ui/votes/VotesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.hotcue.ui.votes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hotcue.R
+
+class VotesFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_votes, container,false)
+
+        return view;
+    }
+}
