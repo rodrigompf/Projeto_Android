8d726ca8b339855e1a13f424ba70d713675677cc
vanori <alexvaarino7@gmail.com>
2024-04-19 10:06:42 +0100

    layout login e layout registo border2 - border laranja de 3dp adição da cor laranja

--- app/src/main/java/com/example/hotcue/MainActivity.kt
+++ app/src/main/java/com/example/hotcue/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.hotcue
 
-import android.app.Notification.Action
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_login)
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword("n@g.com","yepp123");
         }
