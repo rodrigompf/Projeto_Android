78584f2544873c1bf92619d805deaee9e1773569
vanori <alexvaarino7@gmail.com>
2024-04-26 11:10:06 +0100

    template do criar voto inflate do template criar voto no fragmento criar votaçao

--- app/src/main/java/com/example/hotcue/ui/criarVotacao/CriarVotacaoFragment.kt
+++ app/src/main/java/com/example/hotcue/ui/criarVotacao/CriarVotacaoFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.example.hotcue.R
 
@@ -16,9 +18,20 @@ class CriarVotacaoFragment: Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_criar_votacao, container,false)
-
         return view;
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Assuming your button or any view that triggers the creation is called "your_trigger_button"
+        val botao_criar_voto = view.findViewById<AppCompatButton>(R.id.botao_criar_voto)
+        val linear_criar_voto = view.findViewById<LinearLayout>(R.id.linear_criar_voto)
+
+        botao_criar_voto.setOnClickListener {
+
+            val templateCriarVoto = LayoutInflater.from(view.context).inflate(R.layout.template_criar_voto, null, false)
+            linear_criar_voto.addView(templateCriarVoto)
+        }
+    }
 }
