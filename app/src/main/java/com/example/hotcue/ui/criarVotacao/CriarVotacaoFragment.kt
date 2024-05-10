01cb61b635ce7f72dac3d9820bc35260402e3882
rodrigompf <111639927+rodrigompf@users.noreply.github.com>
2024-05-10 11:56:08 +0100

    mehmehmehmhe

--- app/src/main/java/com/example/hotcue/ui/criarVotacao/CriarVotacaoFragment.kt
+++ app/src/main/java/com/example/hotcue/ui/criarVotacao/CriarVotacaoFragment.kt
@@ -5,8 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.example.hotcue.R
@@ -15,17 +18,6 @@ import com.google.firebase.firestore.FirebaseFirestore
 class CriarVotacaoFragment : Fragment() {
     private val db = FirebaseFirestore.getInstance()
 
-    data class Vote(
-        val vtFinalizada: Boolean,
-        val vtIdentificacao: String,
-        val vtTipo: String,
-        val vtUtilizador: String,
-        val vtVotos: Char
-    )
-
-    data class Votecampos(
-        val vt_campo: String
-    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,22 +29,59 @@ class CriarVotacaoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        FirebaseFirestore.getInstance()
 
+        val etextView = view.findViewById<EditText>(R.id.textView)
         val botaoCriarVoto = view.findViewById<AppCompatButton>(R.id.botao_criar_voto)
         val linearCriarVoto = view.findViewById<LinearLayout>(R.id.linear_criar_voto)
         val botaoEnviarVoto = view.findViewById<AppCompatButton>(R.id.botao_enviar_voto)
-
+        val Titulo = view.findViewById<EditText>(R.id.Titulo)
+        val spinnerTipoVotacao = view.findViewById<Spinner>(R.id.spinner_tipo_votacao)
         botaoCriarVoto.setOnClickListener {
             val templateCriarVoto = LayoutInflater.from(requireContext())
                 .inflate(R.layout.template_criar_voto, null, false)
             linearCriarVoto.addView(templateCriarVoto)
         }
+        // Configurar um listener para o Spinner
+        spinnerTipoVotacao.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
 
         botaoEnviarVoto.setOnClickListener {
-            adicionarVotacao()
+            val stextView = etextView.text.toString().trim()
+            val currentSpinnerValue = spinnerTipoVotacao.selectedItem.toString()
+            val eTitulo = Titulo.text.toString().trim()
+            val userMap = hashMapOf(
+                "Descrição" to stextView,
+                "tipo" to currentSpinnerValue,
+                "Titulo" to eTitulo
+            )
+            db.collection("campos_votacao").document("3").set(userMap)
+                .addOnSuccessListener {
+                    etextView.text.clear()
+                }
+                .addOnFailureListener {
+
+                }
         }
     }
+}
+
+
+/*data class Vote(
+        val vtFinalizada: Boolean,
+        val vtIdentificacao = textView
+        val vtTipo: String,
+        val vtUtilizador: String,
+        val vtVotos: Char
+
+        data class Votecampos(
+        val vt_campo: String
+    )
 
     fun adicionarVotacao() {
         val linearCriarVoto = requireView().findViewById<LinearLayout>(R.id.linear_criar_voto)
@@ -60,32 +89,34 @@ class CriarVotacaoFragment : Fragment() {
             val votoView = linearCriarVoto.getChildAt(i)
             if (votoView is LinearLayout) {
                 val editText = votoView.findViewById<EditText>(R.id.txt_criar_voto)
-                val textoVoto = editText.text.toString()
+                val textoVoto = editText.text.toString().trim()
                 addVoteToFirestore(textoVoto)
             }
         }
     }
 
     fun addVoteToFirestore(textoVoto: String) {
-        Log.d("MeuApp", "Texto do Voto: $textoVoto")
         val vote = hashMapOf(
             "vtFinalizada" to false,
-            "vtIdentificacao" to "", // Você pode definir isso conforme necessário
-            "vtTipo" to "", // Você pode definir isso conforme necessário
-            "vtUtilizador" to "", // Você pode definir isso conforme necessário
+            "vtIdentificacao" to stextView ,
+            "vtTipo" to "",
+            "vtUtilizador" to "",
             "vtVotos" to ""
         )
 
         db.collection("votacoes")
             .add(vote)
             .addOnSuccessListener { documentReference ->
-                // Adicionado com sucesso ao Firestore
             }
             .addOnFailureListener { e ->
-                // Tratar erros
             }
     }
-}
+
+    botaoEnviarVoto.setOnClickListener {
+            adicionarVotacao()
+        }
+    )*/
+
 
 
 
