e70eaccce5d3f9d7ad915ebf7ad514b2e50dcccf
rodrigompf <111639927+rodrigompf@users.noreply.github.com>
2024-05-22 18:05:08 +0100

    So falta o votes

--- app/src/main/java/com/example/hotcue/ui/jogos/JogosFragment.kt
+++ app/src/main/java/com/example/hotcue/ui/jogos/JogosFragment.kt
@@ -1,24 +1,68 @@
 package com.example.hotcue.ui.jogos
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hotcue.Adapter
+import com.example.hotcue.OrientacaoVotos
 import com.example.hotcue.R
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
 class JogosFragment: Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: Adapter
+    private lateinit var db: FirebaseFirestore
+    private lateinit var orientacaoVotos: ArrayList<OrientacaoVotos>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_votesfilme, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_jogos, container,false)
+        recyclerView = view.findViewById(R.id.recyclar)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.setHasFixedSize(true)
 
-        return view;
+        orientacaoVotos = ArrayList()
+        adapter = Adapter(orientacaoVotos)
+        recyclerView.adapter = adapter
+
+        // Initialize Firestore here
+        db = FirebaseFirestore.getInstance()
+
+        EventChangeListener()
+
+        return view
     }
 
+    private fun EventChangeListener() {
+        db.collection("votacoes")
+            .document("Jogos") // Assuming "Filmes" is a document, change this according to your Firestore structure
+            .collection("items")
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    Log.e("Firestore error", error.message.toString())
+                    // Handle error, if needed
+                    return@addSnapshotListener
+                }
 
+                value?.let { snapshot ->
+                    for (dc in snapshot.documentChanges) {
+                        if (dc.type == DocumentChange.Type.ADDED) {
+                            val orientacaoVoto = dc.document.toObject(OrientacaoVotos::class.java)
+                            orientacaoVotos.add(orientacaoVoto)
+                            adapter.notifyDataSetChanged()
+                        }
+                    }
+                }
+            }
+    }
 }
